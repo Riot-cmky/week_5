5614c8f09b1eeab749e31a45a7f3b1e9571af772
Branch <dlbranch@cps.k12.il.us>
2024-09-24 14:35:48 -0500

    first commit

---
+++ python_basics/lesson_1.py
@@ -0,0 +1,33 @@
+#mkdir means make a directory
+#cd means change directory
+#ls means list directory
+#pwd means print working directory
+#touch means create a file (only works for mac)
+#rm means remove a file
+# pound sign means comment
+# print("Hello, World!")
+# print("One Piece")
+# print("free will")
+# print("age \t 25")
+# # \t means tab
+# print("address")
+# press control ? to comment out multiple lines
+
+#ask for input
+name = input("What is your name?")
+print("Hello,"+ name)
+# ask for age
+#print out age
+#ask for their place of birth
+#print out address
+#ask what school the went to
+#print out school
+age = input("What is your age?")
+print("Your are"+ age + ' years old')
+WDYL = input("Where do you live?")
+print("Your address is"+ WDYL)
+school = input("What school do you go to?")
+print("Your school is"+ school)
+#create a stence or print statement that includes
+#  all of the information that you have collected
+print("Hello," + name ". You are" + age + "years old")
