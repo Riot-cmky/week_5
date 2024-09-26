9ae9adaed881b453b77fdfd4695595609a9a9b17
Branch <dlbranch@cps.k12.il.us>
2024-09-26 14:38:24 -0500

    whatever changes you worked on

--- week_5/sets_tuples_lists.py
+++ week_5/sets_tuples_lists.py
@@ -27,23 +27,23 @@ fruits = ["apple", "orange", "banana", "coconut", "tomato", "mangao", "watermelo
 # fruits.append("blueberry")
 # adds a element to the end of the list
 
-fruits.remove("apple")
+# fruits.remove("apple")
 # removes element
 
-fruits.insert(0,"kiwi")
+# fruits.insert(0,"kiwi")
 #puts something right there and moves everything over
 
-fruits.sort()
+# fruits.sort()
 # It orders everything in alphabetical order
 
-fruits.reverse()
+# fruits.reverse()
 # reverse alphabetical order
 
-fruits.clear()
+# fruits.clear()
 #removes everything
 
-fruits.index("apple")
-print(fruits)
+# fruits.index("apple")
+# print(fruits)
 
 # print(fruits[::2]) 
 # every second element
@@ -60,3 +60,31 @@ print(fruits)
 # interation. 
 
 
+cars = ["Ford", "Volvo", "BMW"]
+# add 4 new cars to the list
+cars.append("Bugatti")
+cars.append("Ram")
+cars.append("Dodge")
+cars.append("Mitsubishi")
+# print out the list of cars in a f-string
+# that says "The cars in the list are: "
+print(f"The cars in the list are: + {cars}")
+# replace the last element in the list with another car
+cars[-1] = "Fiat"
+# print out the list of cars in an f-string
+print(f"The cars in the list are: + {cars}")
+
+# replace the 3rd elemnet in the list with another car
+cars[2] = "alfa romeo"
+# print out the list of cars in an f-strong
+print(f"The cars in the list are: + {cars}")
+
+#insert a new care in the 2nd position
+cars.insert(0,"Maserati")
+#print out the list of cars in an f-string
+print(f"The cars in the list are: + {cars}")
+
+#remove the 3rd element in the list
+cars.remove("Volvo")
+# print out the list of cars in an f-string
+print(f"The cars in the list are: + {cars}")
